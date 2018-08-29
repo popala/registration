32f83f138604358540968518c712d1468ce3b453
Pawel Opala <modelarz@gmail.com>
2018-08-29 10:35:37 +0200

    Adding MySQL support

---
+++ Program/data/MySqlUtil.cs
@@ -0,0 +1,162 @@
+﻿using Rejestracja.Utils;
+using Rejestracja.Data.Objects;
+using System;
+using MySql.Data.MySqlClient;
+using System.Text;
+
+namespace Rejestracja.Data {
+    class MySqlUtil {
+
+        public static string testConnection(string server, string database, string user, string password, int port, string additionalOptions) {
+            String connectionString = String.Format("server={0};user={1};port={2};password={3};database={4};{5}", server, user, port, password, database, additionalOptions);
+            StringBuilder sb = new StringBuilder();
+
+            using (MySqlConnection cn = new MySqlConnection(connectionString))
+            using (MySqlCommand cm = new MySqlCommand("SHOW TABLES", cn)) {
+                cn.Open();
+                cm.CommandType = System.Data.CommandType.Text;
+
+                using(MySqlDataReader dr = cm.ExecuteReader()) {
+                    while (dr.Read()) {
+                        sb.AppendLine(dr[0].ToString());
+                    }
+                }
+            }
+            return sb.ToString();
+        }
+
+        public static void createDatabase(string server, string database, string user, string password, int port, string additionalOptions) {
+            String connectionString = String.Format("server={0};user={1};port={2};password={3};{4}", server, user, port, password, additionalOptions);
+
+            using (MySqlConnection cn = new MySqlConnection(connectionString))
+            using (MySqlCommand cm = new MySqlCommand("CREATE DATABASE IF NOT EXISTS @database", cn)) {
+                cn.Open();
+                cm.CommandType = System.Data.CommandType.Text;
+                cm.Parameters.AddWithValue("@database", database);
+                cm.ExecuteNonQuery();
+            }
+        }
+
+        public static void createTables() {
+            StringBuilder sb = new StringBuilder();
+            sb.Append(
+                String.Format(
+                    @"CREATE TABLE Classes(
+                        Id INTEGER PRIMARY KEY,
+                        Name VARCHAR({0}) NOT NULL,
+                        RegistrationTemplate VARCHAR({1}) NULL,
+                        JudgingFormTemplate VARCHAR({1}) NULL,
+                        DiplomaTemplate VARCHAR({1}) NULL,
+                        ScoringCardType TINYINT NOT NULL DEFAULT 0,
+                        UseCustomAgeGroups TINYINT NOT NULL DEFAULT 0,
+                        ClassificationType TINYINT NOT NULL DEFAULT 0,
+                        UsePointRange TINYINT NOT NULL DEFAULT 0,
+                        PointRanges VARCHAR({2}) NULL,
+                        UseDistinctions TINYINT NOT NULL DEFAULT 0);
+                    CREATE UNIQUE INDEX Idx_Class_Name ON Classes(Name);",
+                    Class.MAX_NAME_LENGTH,
+                    1024,
+                    256
+                )
+            );
+
+            sb.Append(
+                String.Format(
+                    @"CREATE TABLE Modelers(
+                        Id INTEGER PRIMARY KEY,
+                        FirstName VARCHAR({0}) NOT NULL,
+                        LastName VARCHAR({0}) NOT NULL,
+                        ClubName VARCHAR({0}) NULL,
+                        YearOfBirth SMALLINT NOT NULL DEFAULT 0,
+                        Email VARCHAR({1}));
+                    CREATE INDEX Idx_Modeler_Name ON Modelers(FirstName, LastName, Email);",
+                    Modeler.MAX_NAME_LENGTH,
+                    Modeler.MAX_EMAIL_LENGTH
+                )
+            );
+
+            sb.Append(
+                String.Format(
+                    @"CREATE TABLE Models(
+                        Id INTEGER PRIMARY KEY,
+                        Name VARCHAR({0}),
+                        Publisher VARCHAR({1}),
+                        Scale VARCHAR({2}),
+                        ModelerId INTEGER NOT NULL,
+                        FOREIGN KEY fk_modeler_id (ModelerId)
+                        REFERENCES Modelers(Id)
+                        ON DELETE cascade
+                        ON UPDATE cascade);
+                    CREATE INDEX Idx_Models_MdlrId ON Models(ModelerId);",
+                    Model.MAX_NAME_LENGTH,
+                    Publisher.MAX_NAME_LENGTH,
+                    Scale.MAX_NAME_LENGTH
+                )
+            );
+
+            String query = 
+@"CREATE TABLE Registration(
+    Id INTEGER PRIMARY KEY,
+    TmStamp DATETIME NOT NULL,
+    ModelId INTEGER NOT NULL REFERENCES Models(Id),
+    CategoryId INTEGER NOT NULL DEFAULT -1,
+    CategoryName TEXT NULL,
+    AgeGroupName TEXT NULL REFERENCES AgeGroup(Name));
+CREATE UNIQUE INDEX Idx_Reg_ModelCat ON Registration(ModelId, CategoryId);
+
+CREATE TABLE Results(
+	ResultId INTEGER PRIMARY KEY,
+	RegistrationId INTEGER NOT NULL REFERENCES Registration(Id),
+	AwardId INTEGER NULL REFERENCES SpecialAwards(Id),
+	Place INTEGER NULL);
+
+CREATE TABLE AgeGroups(
+    Id INTEGER PRIMARY KEY,
+    ClassId INTEGER NOT NULL,
+    Name TEXT NOT NULL,
+    Age INTEGER NOT NULL);
+CREATE UNIQUE INDEX Idx_AgeGroups_Name_ClsId ON AgeGroups(ClassId, Name);
+
+CREATE TABLE Categories(
+    Id INTEGER PRIMARY KEY,
+    Code TEXT NOT NULL,
+    Name TEXT,
+    ModelClass TEXT,
+    DisplayOrder INTEGER NOT NULL);
+CREATE INDEX Idx_MC_Code ON Categories(Code);
+CREATE INDEX Idx_MC_Class ON Categories(ModelClass);
+
+CREATE TABLE Scales(
+    Id INTEGER PRIMARY KEY,
+    Name TEXT NOT NULL);
+CREATE UNIQUE INDEX Idx_Scales_Name ON Scales(Name);
+
+CREATE TABLE Publishers(
+    Id INTEGER  PRIMARY KEY,
+    Name TEXT NOT NULL);
+CREATE INDEX Idx_Pub_Name ON Publishers(Name);
+
+CREATE TABLE Awards(
+    Id INTEGER PRIMARY KEY,
+    Title TEXT NOT NULL,
+    DisplayOrder INTEGER NOT NULL);
+CREATE INDEX Idx_Aw_Title ON Awards(Title);
+
+CREATE TABLE Options(
+    Name TEXT NOT NULL PRIMARY KEY,
+    Value TEXT NOT NULL);
+CREATE UNIQUE INDEX Idx_Opt_Name ON Options(Name);
+
+CREATE TABLE Version(Version REAL NOT NULL);
+CREATE UNIQUE INDEX Idx_Ver_Ver ON Version(Version);
+INSERT INTO Version(Version) VALUES(.941);";
+
+            using (MySqlConnection cn = new MySqlConnection(Resources.getConnectionString()))
+            using (MySqlCommand cm = new MySqlCommand(query, cn)) {
+                cn.Open();
+                cm.CommandType = System.Data.CommandType.Text;
+                cm.ExecuteNonQuery();
+            }
+        }
+    }
+}
--- Program/frmNewDataFile.Designer.cs
+++ Program/frmNewDataFile.Designer.cs
@@ -40,9 +40,24 @@
             this.tpOpenFile = new System.Windows.Forms.TabPage();
             this.lvFileList = new System.Windows.Forms.ListView();
             this.lblError = new System.Windows.Forms.Label();
+            this.tpOpenDatabase = new System.Windows.Forms.TabPage();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtServer = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtDatabase = new System.Windows.Forms.TextBox();
+            this.btnTest = new System.Windows.Forms.Button();
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
             this.tabControl1.SuspendLayout();
             this.tpNewFile.SuspendLayout();
             this.tpOpenFile.SuspendLayout();
+            this.tpOpenDatabase.SuspendLayout();
             this.SuspendLayout();
             // 
             // btnOk
@@ -73,6 +88,7 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.tabControl1.Controls.Add(this.tpNewFile);
             this.tabControl1.Controls.Add(this.tpOpenFile);
+            this.tabControl1.Controls.Add(this.tpOpenDatabase);
             this.tabControl1.Location = new System.Drawing.Point(0, 0);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
@@ -92,7 +108,7 @@
             this.tpNewFile.Padding = new System.Windows.Forms.Padding(3);
             this.tpNewFile.Size = new System.Drawing.Size(528, 229);
             this.tpNewFile.TabIndex = 0;
-            this.tpNewFile.Text = "Stwórz Nowy Plik";
+            this.tpNewFile.Text = "Nowy Plik";
             this.tpNewFile.UseVisualStyleBackColor = true;
             // 
             // label2
@@ -153,7 +169,7 @@
             this.tpOpenFile.Padding = new System.Windows.Forms.Padding(3);
             this.tpOpenFile.Size = new System.Drawing.Size(528, 229);
             this.tpOpenFile.TabIndex = 1;
-            this.tpOpenFile.Text = "Otwórz Istniejący Plik";
+            this.tpOpenFile.Text = "Istniejący Plik";
             this.tpOpenFile.UseVisualStyleBackColor = true;
             // 
             // lvFileList
@@ -182,6 +198,152 @@
             this.lblError.TabIndex = 5;
             this.lblError.Text = "lblError";
             // 
+            // tpOpenDatabase
+            // 
+            this.tpOpenDatabase.Controls.Add(this.label9);
+            this.tpOpenDatabase.Controls.Add(this.textBox1);
+            this.tpOpenDatabase.Controls.Add(this.txtPort);
+            this.tpOpenDatabase.Controls.Add(this.btnCreate);
+            this.tpOpenDatabase.Controls.Add(this.btnTest);
+            this.tpOpenDatabase.Controls.Add(this.label6);
+            this.tpOpenDatabase.Controls.Add(this.txtDatabase);
+            this.tpOpenDatabase.Controls.Add(this.label5);
+            this.tpOpenDatabase.Controls.Add(this.txtPassword);
+            this.tpOpenDatabase.Controls.Add(this.label4);
+            this.tpOpenDatabase.Controls.Add(this.txtUser);
+            this.tpOpenDatabase.Controls.Add(this.label3);
+            this.tpOpenDatabase.Controls.Add(this.txtServer);
+            this.tpOpenDatabase.Location = new System.Drawing.Point(4, 22);
+            this.tpOpenDatabase.Name = "tpOpenDatabase";
+            this.tpOpenDatabase.Size = new System.Drawing.Size(528, 229);
+            this.tpOpenDatabase.TabIndex = 3;
+            this.tpOpenDatabase.Text = "Baza MySQL";
+            this.tpOpenDatabase.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(44, 26);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 13);
+            this.label3.TabIndex = 19;
+            this.label3.Text = "Serwer/Port:";
+            // 
+            // txtServer
+            // 
+            this.txtServer.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtServer.Location = new System.Drawing.Point(117, 23);
+            this.txtServer.MaxLength = 256;
+            this.txtServer.Name = "txtServer";
+            this.txtServer.Size = new System.Drawing.Size(291, 20);
+            this.txtServer.TabIndex = 18;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(46, 52);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 13);
+            this.label4.TabIndex = 21;
+            this.label4.Text = "Użytkownik:";
+            // 
+            // txtUser
+            // 
+            this.txtUser.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtUser.Location = new System.Drawing.Point(117, 49);
+            this.txtUser.MaxLength = 256;
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(291, 20);
+            this.txtUser.TabIndex = 20;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(72, 78);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(39, 13);
+            this.label5.TabIndex = 23;
+            this.label5.Text = "Hasło:";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtPassword.Location = new System.Drawing.Point(117, 75);
+            this.txtPassword.MaxLength = 256;
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(291, 20);
+            this.txtPassword.TabIndex = 22;
+            this.txtPassword.UseSystemPasswordChar = true;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(77, 104);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(34, 13);
+            this.label6.TabIndex = 25;
+            this.label6.Text = "Baza:";
+            // 
+            // txtDatabase
+            // 
+            this.txtDatabase.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtDatabase.Location = new System.Drawing.Point(117, 101);
+            this.txtDatabase.MaxLength = 256;
+            this.txtDatabase.Name = "txtDatabase";
+            this.txtDatabase.Size = new System.Drawing.Size(291, 20);
+            this.txtDatabase.TabIndex = 24;
+            // 
+            // btnTest
+            // 
+            this.btnTest.Location = new System.Drawing.Point(414, 187);
+            this.btnTest.Name = "btnTest";
+            this.btnTest.Size = new System.Drawing.Size(75, 23);
+            this.btnTest.TabIndex = 26;
+            this.btnTest.Text = "Testuj";
+            this.btnTest.UseVisualStyleBackColor = true;
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Location = new System.Drawing.Point(333, 187);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(75, 23);
+            this.btnCreate.TabIndex = 26;
+            this.btnCreate.Text = "Stwórz";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            // 
+            // txtPort
+            // 
+            this.txtPort.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtPort.Location = new System.Drawing.Point(414, 23);
+            this.txtPort.MaxLength = 256;
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(75, 20);
+            this.txtPort.TabIndex = 27;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(33, 152);
+            this.textBox1.MaxLength = 256;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(456, 20);
+            this.textBox1.TabIndex = 28;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(30, 136);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(297, 13);
+            this.label9.TabIndex = 30;
+            this.label9.Text = "Dodatkowe parametry połączenia (używaj na własne ryzyko):";
+            // 
             // frmNewDataFile
             // 
             this.AcceptButton = this.btnOk;
@@ -201,6 +363,8 @@
             this.tpNewFile.ResumeLayout(false);
             this.tpNewFile.PerformLayout();
             this.tpOpenFile.ResumeLayout(false);
+            this.tpOpenDatabase.ResumeLayout(false);
+            this.tpOpenDatabase.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -220,5 +384,19 @@
         private System.Windows.Forms.ListView lvFileList;
         private System.Windows.Forms.Label lblError;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TabPage tpOpenDatabase;
+        private System.Windows.Forms.Button btnCreate;
+        private System.Windows.Forms.Button btnTest;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtDatabase;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtUser;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtServer;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtPort;
     }
 }
