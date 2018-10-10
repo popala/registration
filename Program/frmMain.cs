d1bb5694474cbfc4513849688212ed25eda4bea9
Pawel Opala <modelarz@gmail.com>
2018-10-10 12:39:24 +0200

    Fixed problems with YOB being a date field

--- Program/frmMain.cs
+++ Program/frmMain.cs
@@ -512,7 +512,7 @@ namespace Rejestracja {
                     highlightErrorCell(item, 7);
                 }
 
-                if (checkAgeGroupAge) {
+                if (agFound.Length > 0 && checkAgeGroupAge) {
                     int age = year - int.Parse(item.SubItems[5].Text);
                     if (age > agFound[0].upperAge || age < agFound[0].bottomAge) {
                         if (sb.Length == 0) {
